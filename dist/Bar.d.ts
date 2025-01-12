97e219d6ad1f998f042818de44c08b74a4683fa2
Александр Земцев <azemtsev@nanbots.com>
2025-01-13 01:46:15 +0300

    React native 0.73 +, react 18.2.0 dist

---
+++ dist/Bar.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+declare type BarProps = {
+    barStyle?: object;
+};
+export declare const Bar: ({ barStyle }: BarProps) => React.JSX.Element;
+export {};
---
+++ dist/Close.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+declare type CloseProps = {
+    onPress: () => void;
+    rootStyle?: object;
+    iconStyle?: object;
+};
+export declare const Close: ({ onPress, rootStyle, iconStyle }: CloseProps) => React.JSX.Element;
+export {};
